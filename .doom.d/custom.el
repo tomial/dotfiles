f0c658d189248070a903c52be42906b46a512fb3
tomial <b1250284002@hotmail.com>
2022-01-07 17:01:02 +0800

    switch to doom emacs

---
+++ .doom.d/custom.el
@@ -0,0 +1,18 @@
+;;; custom.el ---                                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Aaron
+
+;; Author: Aaron <b1250284002@hotmail.com>
+;; Keywords:
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(spinner gnu-elpa-keyring-update)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
