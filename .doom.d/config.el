2767bd2683cb3df5d3d1a4fad638bcdf20bd8786
tomial <b1250284002@hotmail.com>
2022-03-25 15:42:32 +0800

    update doom emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -19,9 +19,9 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Victor Mono" :size 20 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Victor Mono" :size 20)
-      doom-unicode-font (font-spec :family "JetBrainsMono NF" :size 18))
+(setq doom-font (font-spec :family "Fira Code" :size 20 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Fira Code" :size 20)
+      doom-unicode-font (font-spec :family "JetBrainsMono NF" :size 20))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -9,7 +9,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(spinner gnu-elpa-keyring-update)))
+ '(package-selected-packages '(spinner gnu-elpa-keyring-update))
+ '(safe-local-variable-values '((encoding . utf-8)))
+  (setq default-text-properties '(line-spacing 0.25 line-height 1.3))
+
+  (defun set-bigger-spacing ()
+  (setq-local default-text-properties '(line-spacing 0.25 line-height 1.3)))
+  (add-hook 'text-mode-hook 'set-bigger-spacing)
+  (add-hook 'prog-mode-hook 'set-bigger-spacing)
+
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
