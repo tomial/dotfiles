a329f8caa5a7373522dec788546133f72c7044e8
aarl <b1250284002@hotmail.com>
2026-02-25 14:53:41 +0800

    nvim: fix codecompanion markdown render issue

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -46,16 +46,6 @@ return {
       {
         "MeanderingProgrammer/render-markdown.nvim",
         ft = { "markdown", "codecompanion" }
-      },
-      {
-        "OXY2DEV/markview.nvim",
-        lazy = false,
-        opts = {
-          preview = {
-            filetypes = { "markdown", "codecompanion" },
-            ignore_buftypes = {},
-          },
-        },
       }
     },
     opts = {
