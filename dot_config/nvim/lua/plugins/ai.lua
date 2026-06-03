fa6e3583b905411c74d0836a803ebcaa9ba2e928
aarl <b1250284002@hotmail.com>
2026-06-03 16:13:08 +0800

    update nvim configs

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -6,76 +6,76 @@ vim.keymap.set("v", "<leader>cc", ":'<,'>CodeCompanion<CR>")
 vim.keymap.set("n", "<leader>pi", ":PasteImage<CR>")
 
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "InsertEnter",
-    opts = {
-      suggestion = {
-        auto_trigger = true,
-        debounce = 1000,
-        keymap = {
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<Esc>",
-        },
-      },
-    },
-  },
-  {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      {
-        "HakonHarnes/img-clip.nvim",
-        opts = {
-          filetypes = {
-            codecompanion = {
-              prompt_for_file_name = false,
-              template = "[Image]($FILE_PATH)",
-              use_absolute_path = true,
-            },
-          },
-        },
-      },
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        ft = { "markdown", "codecompanion" }
-      }
-    },
-    opts = {
-      opts = {
-        log_level = "TRACE",
-        language = "Simplified Chinese 简体中文"
-      },
-      interactions = {
-        chat = {
-          adapter = {
-            name = "claude_code",
-            model = "MiniMax-M2.5",
-          },
-        },
-        inline = {
-          keymaps = {
-            accept_change = {
-              modes = { n = "ccy" }, -- Remember this as DiffAccept
-            },
-            reject_change = {
-              modes = { n = "ccn" }, -- Remember this as DiffReject
-            },
-            always_accept = {
-              modes = { n = "cca" }, -- Remember this as DiffYolo
-            },
-          },
-        }
-      },
-      adapters = {
-        claude_code = "acp",
-      },
-    }
-  }
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   event = "InsertEnter",
+  --   opts = {
+  --     suggestion = {
+  --       auto_trigger = true,
+  --       debounce = 1000,
+  --       keymap = {
+  --         accept_word = false,
+  --         accept_line = false,
+  --         next = "<M-]>",
+  --         prev = "<M-[>",
+  --         dismiss = "<Esc>",
+  --       },
+  --     },
+  --   },
+  -- }
+  -- {
+  --   "olimorris/codecompanion.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     {
+  --       "HakonHarnes/img-clip.nvim",
+  --       opts = {
+  --         filetypes = {
+  --           codecompanion = {
+  --             prompt_for_file_name = false,
+  --             template = "[Image]($FILE_PATH)",
+  --             use_absolute_path = true,
+  --           },
+  --         },
+  --       },
+  --     },
+  --     {
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       ft = { "markdown", "codecompanion" }
+  --     }
+  --   },
+  --   opts = {
+  --     opts = {
+  --       log_level = "TRACE",
+  --       language = "Simplified Chinese 简体中文"
+  --     },
+  --     interactions = {
+  --       chat = {
+  --         adapter = {
+  --           name = "claude_code",
+  --           model = "MiniMax-M2.5",
+  --         },
+  --       },
+  --       inline = {
+  --         keymaps = {
+  --           accept_change = {
+  --             modes = { n = "ccy" }, -- Remember this as DiffAccept
+  --           },
+  --           reject_change = {
+  --             modes = { n = "ccn" }, -- Remember this as DiffReject
+  --           },
+  --           always_accept = {
+  --             modes = { n = "cca" }, -- Remember this as DiffYolo
+  --           },
+  --         },
+  --       }
+  --     },
+  --     adapters = {
+  --       claude_code = "acp",
+  --     },
+  --   }
+  -- }
 }
--- dot_config/nvim/lua/plugins/themes.lua
+++ dot_config/nvim/lua/plugins/themes.lua
@@ -10,13 +10,13 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
-      flavour = "frappe",
+      flavour = "mocha",
       transparent_background = true,
       auto_integrations = true,
     },
     config = function(_, opts)
-      -- require("catppuccin").setup(opts)
-      -- vim.cmd.colorscheme("catppuccin")
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
     end
   },
   {
@@ -24,8 +24,8 @@ return {
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("koda").setup({ transparent = true })
-      vim.cmd("colorscheme koda-moss")
+      -- require("koda").setup({ transparent = true })
+      -- vim.cmd("colorscheme koda-moss")
     end,
   },
   {
